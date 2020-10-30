65b5626d21e2f5e98d18a5cd4c5b954e9456f98d
Kartheek Solipuram <kartheek.s@gmail.com>
2020-10-29 23:07:00 -0700

    Update uconfig.h to enable suggested icu4c customizations for swift build.

--- icu4c/source/common/unicode/uconfig.h
+++ icu4c/source/common/unicode/uconfig.h
@@ -1,4 +1,8 @@
-// © 2016 and later: Unicode, Inc. and others.
+/* ICU customizations: put these lines at the top of uconfig.h */
+ /* -DU_HAVE_LIB_SUFFIX=1 */
+#define U_HAVE_LIB_SUFFIX 1
+ /* -DU_LIB_SUFFIX_C_NAME=_swift */
+#define U_LIB_SUFFIX_C_NAME _swift // © 2016 and later: Unicode, Inc. and others.
 // License & terms of use: http://www.unicode.org/copyright.html
 /*  
 **********************************************************************
